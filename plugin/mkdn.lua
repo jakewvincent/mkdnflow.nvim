9c9a31c4d7afc17116fff5a6de4e5deef2a2af66
Jake Vincent <jakewvincent@gmail.com>
2021-07-30 19:38:17 -0700

    Add license

--- plugin/mkdn.lua
+++ plugin/mkdn.lua
@@ -1,30 +1,41 @@
--- License here
+-- mkdn.nvim (Tools for personal markdown notebook management)
+-- Copyright (C) 2021 Jake W. Vincent <https://github.com/jakewvincent>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+-- Only define these commands if the plugin hasn't already been loaded
 if vim.fn.exists('g:loaded_mkdn') == 0 then
 
--- Save user coptions
-local save_cpo = vim.api.nvim_get_option('cpoptions')
--- Retrieve defaults
-local cpo_defaults = vim.api.nvim_get_option_info('cpoptions')['default']
--- Set to defaults
-vim.api.nvim_set_option('cpoptions', cpo_defaults)
+    -- Save user coptions
+    local save_cpo = vim.api.nvim_get_option('cpoptions')
+    -- Retrieve defaults
+    local cpo_defaults = vim.api.nvim_get_option_info('cpoptions')['default']
+    -- Set to defaults
+    vim.api.nvim_set_option('cpoptions', cpo_defaults)
 
--- Define commands
-vim.api.nvim_exec("command! MkdnNextLink lua require('mkdn').cursor.toNextLink()", true)
-vim.api.nvim_exec("command! MkdnPrevLink lua require('mkdn').cursor.toPrevLink()", true)
+    -- Define commands
+    vim.api.nvim_exec("command! MkdnNextLink lua require('mkdn').cursor.toNextLink()", true)
+    vim.api.nvim_exec("command! MkdnPrevLink lua require('mkdn').cursor.toPrevLink()", true)
 
--- Test commands
-vim.api.nvim_exec("command! MkdnGetPath lua require('mkdn').files.getPath()", true)
-vim.api.nvim_exec("command! MkdnFollowPath lua require('mkdn').files.followPath()", true)
-vim.api.nvim_exec("command! MkdnTestNav lua require('mkdn').files.testNavFunc()", true)
-vim.api.nvim_exec("command! MkdnGetWord lua require('mkdn').files.createLink()", true)
+    -- Test commands
+    vim.api.nvim_exec("command! MkdnGetPath lua require('mkdn').files.getPath()", true)
+    vim.api.nvim_exec("command! MkdnFollowPath lua require('mkdn').files.followPath()", true)
+    vim.api.nvim_exec("command! MkdnCreateLink lua require('mkdn').files.createLink()", true)
 
---nnoremap <Tab> :MkdnNextLink<CR>
---nnoremap <S-Tab> :MkdnPrevLink<CR>
+    -- Return coptions to user values
+    vim.api.nvim_set_option('cpoptions', save_cpo)
 
--- Return coptions to user's value
-vim.api.nvim_set_option('cpoptions', save_cpo)
-
--- Loaded
-vim.api.nvim_set_var('loaded_mkdn', true)
+    -- Record that the plugin has been loaded
+    vim.api.nvim_set_var('loaded_mkdn', true)
 end
