349e051d90cf21373f85fe07b1776f5c179c652e
Jake Vincent <jakewvincent@gmail.com>
2021-08-11 10:20:10 -0700

    Define command for backwards navigation

--- plugin/mkdnflow.lua
+++ plugin/mkdnflow.lua
@@ -27,11 +27,10 @@ if vim.fn.exists('g:loaded_mkdnflow') == 0 then
     -- Define commands
     vim.api.nvim_exec("command! MkdnNextLink lua require('mkdnflow').cursor.toNextLink()", true)
     vim.api.nvim_exec("command! MkdnPrevLink lua require('mkdnflow').cursor.toPrevLink()", true)
-
-    -- Test commands
     vim.api.nvim_exec("command! MkdnGetPath lua require('mkdnflow').files.getPath()", true)
     vim.api.nvim_exec("command! MkdnFollowPath lua require('mkdnflow').files.followPath()", true)
     vim.api.nvim_exec("command! MkdnCreateLink lua require('mkdnflow').files.createLink()", true)
+    vim.api.nvim_exec("command! MkdnGoBack lua require('mkdnflow').files.goBack()", true)
 
     -- Return coptions to user values
     vim.api.nvim_set_option('cpoptions', save_cpo)
