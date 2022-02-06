8f596ef32bbf5e0621d840f13643bc221cc77eb6
Jake [C] Vincent <jakevinc@amazon.com>
2022-02-06 05:54:47 -0800

    License year update

--- plugin/mkdnflow.lua
+++ plugin/mkdnflow.lua
@@ -1,5 +1,5 @@
 -- mkdnflow.nvim (Tools for personal markdown notebook navigation and management)
--- Copyright (C) 2021 Jake W. Vincent <https://github.com/jakewvincent>
+-- Copyright (C) 2022 Jake W. Vincent <https://github.com/jakewvincent>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
