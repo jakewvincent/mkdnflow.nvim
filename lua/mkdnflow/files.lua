3b9a2a099d2aca56a0649be8417cbd15d1d93125
Jake Vincent <jakewvincent@gmail.com>
2022-04-16 13:42:05 -0700

    Fit code & comments within 80-90 character line length for legibility

--- lua/mkdnflow/files.lua
+++ lua/mkdnflow/files.lua
@@ -1,4 +1,4 @@
--- mkdnflow.nvim (Tools for personal markdown notebook navigation and management)
+-- mkdnflow.nvim (Tools for fluent markdown notebook navigation and management)
 -- Copyright (C) 2022 Jake W. Vincent <https://github.com/jakewvincent>
 --
 -- This program is free software: you can redistribute it and/or modify
@@ -47,17 +47,19 @@ local get_path = function()
 
     -- Get the indices of the links in the line
     local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false) -- Get the line text
-    local link_pattern = '%b[](%b())'                             -- What links look like
-    local bib_pattern = '[^%a%d]-(@[%a%d_%.%-\']+)[%s%p%c]?'               -- What bibliographic citations look like
-    local indices = {}                                              -- Table for match indices
-    local last_fin = 1                                              -- Last end index
+    local link_pattern = '%b[](%b())' -- What links look like
+    local bib_pattern = '[^%a%d]-(@[%a%d_%.%-\']+)[%s%p%c]?' -- Bib. citation pattern
+    local indices = {} -- Table for match indices
+    local last_fin = 1 -- Last end index
     local link_type = nil
     local unfound = true
-    -- TODO: Move the check overlap bit, which is repeated twice, to a function definition here, then call the function twice
+    -- TODO: Move the check overlap bit, which is repeated twice, to a function
+    -- definition here, then call the function twice
     while unfound do
         -- Get the indices of any match on the current line
         local com, fin = string.find(line[1], link_pattern, last_fin)
-        -- Check if there's a match that begins after the fin from the previous iteration of the loop
+        -- Check if there's a match that begins after the fin from the previous
+        -- iteration of the loop
         if com and fin then
             -- If there is, check if the match overlaps with the cursor position
             if com - 1 <= col and fin - 1 >= col then
@@ -79,10 +81,16 @@ local get_path = function()
 
     -- Check if a link was found under the cursor
     if unfound == false then
-        -- If one was found and it's an address, get the path part of the match and return it
+        -- If one was found and it's an address, get the path part of the match
+        -- and return it
         if link_type == 'address' then
             local path_pattern = '%b[](%b())'
-            local path = string.sub(string.match(string.sub(line[1], indices['com'], indices['fin']), path_pattern), 2, -2)
+            local path = string.sub(
+                string.match(
+                    string.sub(line[1], indices['com'], indices['fin']),
+                    path_pattern
+                ), 2, -2
+            )
             return(path)
         end
     else -- If one wasn't found, perform another search, this time for citations
@@ -91,7 +99,8 @@ local get_path = function()
             local com, fin = string.find(line[1], bib_pattern, last_fin)
             -- If there was a match, see if the cursor is inside it
             if com and fin then
-                -- If there is, check if the match overlaps with the cursor position
+                -- If there is, check if the match overlaps with the cursor
+                -- position
                 if com - 1 <= col and fin - 1 >= col then
                     -- If it does overlap, save the indices of the match
                     indices = {com = com, fin = fin}
@@ -110,7 +119,11 @@ local get_path = function()
         end
         if unfound == false then
             if link_type == 'citation' then
-                local citation = string.match(string.sub(line[1], indices['com'], indices['fin']), bib_pattern)
+                local citation = string.match(
+                    string.sub(
+                        line[1], indices['com'], indices['fin']
+                    ), bib_pattern
+                )
                 return(citation)
             end
         else
@@ -128,12 +141,64 @@ Private function
 
 --]]
 local is_url = function(string)
-    -- This function based largely on the solution in <https://stackoverflow.com/questions/23590304/finding-a-url-in-a-string-lua-pattern>
+    -- This function based largely on the solution in https://stackoverflow.com/
+    -- questions/23590304/finding-a-url-in-a-string-lua-pattern
     -- Table of top-level domains
-    local tlds = {ac = true, ad = true, ae = true, aero = true, af = true, ag = true, ai = true, al = true, am = true, an = true, ao = true, aq = true, ar = true, arpa = true, as = true, asia = true, at = true, au = true, aw = true, ax = true, az = true, ba = true, bb = true, bd = true, be = true, bf = true, bg = true, bh = true, bi = true, biz = true, bj = true, bm = true, bn = true, bo = true, br = true, bs = true, bt = true, bv = true, bw = true, by = true, bz = true, ca = true, cat = true, cc = true, cd = true, cf = true, cg = true, ch = true, ci = true, ck = true, cl = true, cm = true, cn = true, co = true, com = true, coop = true, cr = true, cs = true, cu = true, cv = true, cx = true, cy = true, cz = true, dd = true, de = true, dj = true, dk = true, dm = true, ['do'] = true, dz = true, ec = true, edu = true, ee = true, eg = true, eh = true, er = true, es = true, et = true, eu = true, fi = true, firm = true, fj = true, fk = true, fm = true, fo = true, fr = true, fx = true, ga = true, gb = true, gd = true, ge = true, gf = true, gh = true, gi = true, gl = true, gm = true, gn = true, gov = true, gp = true, gq = true, gr = true, gs = true, gt = true, gu = true, gw = true, gy = true, hk = true, hm = true, hn = true, hr = true, ht = true, hu = true, id = true, ie = true, il = true, im = true, ['in'] = true, info = true, int = true, io = true, iq = true, ir = true, is = true, it = true, je = true, jm = true, jo = true, jobs = true, jp = true, ke = true, kg = true, kh = true, ki = true, km = true, kn = true, kp = true, kr = true, kw = true, ky = true, kz = true, la = true, lb = true, lc = true, li = true, lk = true, lr = true, ls = true, lt = true, lu = true, lv = true, ly = true, ma = true, mc = true, md = false, me = true, mg = true, mh = true, mil = true, mk = true, ml = true, mm = true, mn = true, mo = true, mobi = true, mp = true, mq = true, mr = true, ms = true, mt = true, mu = true, museum = true, mv = true, mw = true, mx = true, my = true, mz = true, na = true, name = true, nato = true, nc = true, ne = true, net = true, nf = true, ng = true, ni = true, nl = true, no = true, nom = true, np = true, nr = true, nt = true, nu = true, nz = true, om = true, org = true, pa = true, pe = true, pf = true, pg = true, ph = true, pk = true, pl = true, pm = true, pn = true, post = true, pr = true, pro = true, ps = true, pt = true, pw = true, py = true, qa = true, re = true, ro = true, ru = true, rw = true, sa = true, sb = true, sc = true, sd = true, se = true, sg = true, sh = true, si = true, sj = true, sk = true, sl = true, sm = true, sn = true, so = true, sr = true, ss = true, st = true, store = true, su = true, sv = true, sy = true, sz = true, tc = true, td = true, tel = true, tf = true, tg = true, th = true, tj = true, tk = true, tl = true, tm = true, tn = true, to = true, tp = true, tr = true, travel = true, tt = true, tv = true, tw = true, tz = true, ua = true, ug = true, uk = true, um = true, us = true, uy = true, va = true, vc = true, ve = true, vg = true, vi = true, vn = true, vu = true, web = true, wf = true, ws = true, xxx = true, ye = true, yt = true, yu = true, za = true, zm = true, zr = true, zw = true}
+    local tlds = {ac = true, ad = true, ae = true, aero = true, af = true,
+        ag = true, ai = true, al = true, am = true, an = true, ao = true,
+        aq = true, ar = true, arpa = true, as = true, asia = true, at = true,
+        au = true, aw = true, ax = true, az = true, ba = true, bb = true,
+        bd = true, be = true, bf = true, bg = true, bh = true, bi = true,
+        biz = true, bj = true, bm = true, bn = true, bo = true, br = true,
+        bs = true, bt = true, bv = true, bw = true, by = true, bz = true,
+        ca = true, cat = true, cc = true, cd = true, cf = true, cg = true,
+        ch = true, ci = true, ck = true, cl = true, cm = true, cn = true,
+        co = true, com = true, coop = true, cr = true, cs = true, cu = true,
+        cv = true, cx = true, cy = true, cz = true, dd = true, de = true,
+        dj = true, dk = true, dm = true, ['do'] = true, dz = true, ec = true,
+        edu = true, ee = true, eg = true, eh = true, er = true, es = true,
+        et = true, eu = true, fi = true, firm = true, fj = true, fk = true,
+        fm = true, fo = true, fr = true, fx = true, ga = true, gb = true,
+        gd = true, ge = true, gf = true, gh = true, gi = true, gl = true,
+        gm = true, gn = true, gov = true, gp = true, gq = true, gr = true,
+        gs = true, gt = true, gu = true, gw = true, gy = true, hk = true,
+        hm = true, hn = true, hr = true, ht = true, hu = true, id = true,
+        ie = true, il = true, im = true, ['in'] = true, info = true, int = true,
+        io = true, iq = true, ir = true, is = true, it = true, je = true,
+        jm = true, jo = true, jobs = true, jp = true, ke = true, kg = true,
+        kh = true, ki = true, km = true, kn = true, kp = true, kr = true,
+        kw = true, ky = true, kz = true, la = true, lb = true, lc = true,
+        li = true, lk = true, lr = true, ls = true, lt = true, lu = true,
+        lv = true, ly = true, ma = true, mc = true, md = false, me = true,
+        mg = true, mh = true, mil = true, mk = true, ml = true, mm = true,
+        mn = true, mo = true, mobi = true, mp = true, mq = true, mr = true,
+        ms = true, mt = true, mu = true, museum = true, mv = true, mw = true,
+        mx = true, my = true, mz = true, na = true, name = true, nato = true,
+        nc = true, ne = true, net = true, nf = true, ng = true, ni = true,
+        nl = true, no = true, nom = true, np = true, nr = true, nt = true,
+        nu = true, nz = true, om = true, org = true, pa = true, pe = true,
+        pf = true, pg = true, ph = true, pk = true, pl = true, pm = true,
+        pn = true, post = true, pr = true, pro = true, ps = true, pt = true,
+        pw = true, py = true, qa = true, re = true, ro = true, ru = true,
+        rw = true, sa = true, sb = true, sc = true, sd = true, se = true,
+        sg = true, sh = true, si = true, sj = true, sk = true, sl = true,
+        sm = true, sn = true, so = true, sr = true, ss = true, st = true,
+        store = true, su = true, sv = true, sy = true, sz = true, tc = true,
+        td = true, tel = true, tf = true, tg = true, th = true, tj = true,
+        tk = true, tl = true, tm = true, tn = true, to = true, tp = true,
+        tr = true, travel = true, tt = true, tv = true, tw = true, tz = true,
+        ua = true, ug = true, uk = true, um = true, us = true, uy = true,
+        va = true, vc = true, ve = true, vg = true, vi = true, vn = true,
+        vu = true, web = true, wf = true, ws = true, xxx = true, ye = true,
+        yt = true, yu = true, za = true, zm = true, zr = true, zw = true}
 
     -- Table of protocols
-    local protocols = {[''] = 0, ['http://'] = 0, ['https://'] = 0, ['ftp://'] = 0}
+    local protocols = {
+        [''] = 0,
+        ['http://'] = 0,
+        ['https://'] = 0,
+        ['ftp://'] = 0
+    }
 
     -- Table for status of url search
     local finished = {}
@@ -148,7 +213,7 @@ local is_url = function(string)
 
     -- For each group in the match, do some stuff
     for pos_start, url, prot, subd, tld, colon, port, slash, path in
-        string:gmatch'()(([%w_.~!*:@&+$/?%%#-]-)(%w[-.%w]*%.)(%w+)(:?)(%d*)(/?)([%w_.~!*:@&+$/?%%#=-]*))'
+        string:gmatch('()(([%w_.~!*:@&+$/?%%#-]-)(%w[-.%w]*%.)(%w+)(:?)(%d*)(/?)([%w_.~!*:@&+$/?%%#=-]*))')
     do
         if protocols[prot:lower()] == (1 - #slash) * #path and not subd:find'%W%W'
             and (colon == '' or port ~= '' and port + 0 < 65536)
@@ -245,11 +310,11 @@ M.formatLink = function(text, part)
     else
         -- Make a variable for the prefix to use
         local prefix = nil
-        -- If the user wants the prefix evaluated, eval when this function is run
-        -- (i.e. right here)
+        -- If the user wants the prefix evaluated, eval when this function is
+        -- run (i.e., right here)
         if evaluate_prefix then
             prefix = loadstring("return "..new_file_prefix)()
-            -- Otherwise, just use the string provided by the user for the prefix
+            -- Otherwise, use the string provided by the user for the prefix
         else
             prefix = new_file_prefix
         end
@@ -302,7 +367,9 @@ M.createLink = function()
         -- Get the word under the cursor
         local cursor_word = vim.fn.expand('<cword>')
         -- Make a markdown link out of the date and cursor
-        local replacement = {'['..cursor_word..']'..'('..prefix..cursor_word..'.md)'}
+        local replacement = {
+            '['..cursor_word..']'..'('..prefix..cursor_word..'.md)'
+        }
 
         -- Find the (first) position of the matched word in the line
         local left, right = string.find(line, cursor_word, nil, true)
@@ -315,7 +382,9 @@ M.createLink = function()
         end
 
         -- Replace the word under the cursor w/ the formatted link replacement
-        vim.api.nvim_buf_set_text(0, row - 1, left - 1, row - 1, right, replacement)
+        vim.api.nvim_buf_set_text(
+            0, row - 1, left - 1, row - 1, right, replacement
+        )
 
     -- If current mode is 'visual', make link from selection
     elseif mode == 'v' then
@@ -339,10 +408,16 @@ M.createLink = function()
                     vim.fn.visualmode(),
                 (vim.o.selection ~= 'exclusive')
                 )
-            local lines = vim.api.nvim_buf_get_lines(0, start[1], finish[1] + 1, false)
-            lines[1] = lines[1]:sub(region[start[1]][1] + 1, region[start[1]][2])
+            local lines = vim.api.nvim_buf_get_lines(
+                0, start[1], finish[1] + 1, false
+            )
+            lines[1] = lines[1]:sub(
+                region[start[1]][1] + 1, region[start[1]][2]
+            )
             if start[1] ~= finish[1] then
-                lines[#lines] = lines[#lines]:sub(region[finish[1]][1] + 1, region[finish[1]][2])
+                lines[#lines] = lines[#lines]:sub(
+                    region[finish[1]][1] + 1, region[finish[1]][2]
+                )
             end
 
             -- Save the text selection & replace spaces with dashes
@@ -350,7 +425,9 @@ M.createLink = function()
             local replacement = M.formatLink(text)
 
             -- Replace the visual selection w/ the formatted link replacement
-            vim.api.nvim_buf_set_text(0, row - 1, col, com[2] - 1, com[3], replacement)
+            vim.api.nvim_buf_set_text(
+                0, row - 1, col, com[2] - 1, com[3], replacement
+            )
         else
             start = {com[2] - 1, com[3] - 1 + com[4]}
             finish = {row - 1, col}
@@ -363,17 +440,25 @@ M.createLink = function()
                     vim.fn.visualmode(),
                 (vim.o.selection ~= 'exclusive')
                 )
-            local lines = vim.api.nvim_buf_get_lines(0, start[1], finish[1] + 1, false)
-            lines[1] = lines[1]:sub(region[start[1]][1] + 1, region[start[1]][2])
+            local lines = vim.api.nvim_buf_get_lines(
+                0, start[1], finish[1] + 1, false
+            )
+            lines[1] = lines[1]:sub(
+                region[start[1]][1] + 1, region[start[1]][2]
+            )
             if start[1] ~= finish[1] then
-                lines[#lines] = lines[#lines]:sub(region[finish[1]][1] + 1, region[finish[1]][2])
+                lines[#lines] = lines[#lines]:sub(
+                    region[finish[1]][1] + 1, region[finish[1]][2]
+                )
             end
 
             -- Save the text selection
             local text = table.concat(lines)
             local replacement = M.formatLink(text)
             -- Replace the visual selection w/ the formatted link replacement
-            vim.api.nvim_buf_set_text(0, com[2] - 1, com[3] - 1, row - 1, col + 1, replacement)
+            vim.api.nvim_buf_set_text(
+                0, com[2] - 1, com[3] - 1, row - 1, col + 1, replacement
+            )
         end
 
     end
@@ -392,7 +477,9 @@ local does_exist = function(path, type)
     if this_os == "Linux" or this_os == "POSIX" or this_os == "Darwin" then
 
         -- Use the shell to determine if the path exists
-        local handle = io.popen('if [ -'..type..' "'..path..'" ]; then echo true; else echo false; fi')
+        local handle = io.popen(
+            'if [ -'..type..' "'..path..'" ]; then echo true; else echo false; fi'
+        )
         local exists = handle:read('*l')
         io.close(handle)
 
@@ -615,8 +702,10 @@ M.followPath = function(path)
                 end
 
                 -- If the file exists, handle it; otherwise, print a warning
-                -- Don't want to use the shell-escaped version; it will throw a false alert if there are escape chars
-                if does_exist(se_paste, "f") == false and does_exist(se_paste, "d") == false then
+                -- Don't want to use the shell-escaped version; it will throw a
+                -- false alert if there are escape chars
+                if does_exist(se_paste, "f") == false and
+                   does_exist(se_paste, "d") == false then
                     print("⬇️ : "..se_paste.." doesn't seem to exist!")
                 else
                     path_handler(se_paste)
@@ -654,8 +743,11 @@ M.followPath = function(path)
         elseif path_type(path) == 'anchor' then
             require('mkdnflow.cursor').toHeading(path)
         elseif path_type(path) == 'citation' then
-            -- Pass to the citation_handler function from bib.lua to get highest-priority field in bib entry (if it exists)
-            local field = require('mkdnflow.bib').citationHandler(escape_lua_chars(path))
+            -- Pass to the citation_handler function from bib.lua to get
+            -- highest-priority field in bib entry (if it exists)
+            local field = require('mkdnflow.bib').citationHandler(
+                escape_lua_chars(path)
+            )
             -- Use this function to do sth with the information returned (if any)
             if field then
                 M.followPath(field)
@@ -686,7 +778,8 @@ M.goBack = function()
         vim.api.nvim_command("buffer "..prev_buf)
         -- Pop the buffer we just navigated to off the top of the stack
         buffer_stack.pop('main')
-        -- return a boolean if goback succeeded (for users who want <bs> to do sth else if goback isn't possible)
+        -- return a boolean if goback succeeded (for users who want <bs> to do
+        -- sth else if goback isn't possible)
         return(true)
     else
         print('⬇️ : Can\'t go back any further!')
