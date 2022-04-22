75c8ecc5c81bd048b6cd452f30ebde54db258165
Jake Vincent <jakewvincent@gmail.com>
2022-04-22 13:32:54 -0700

    Add compatibility function to convert 'links_relative_to' to required format and ask user to update config

--- lua/mkdnflow/compat.lua
+++ lua/mkdnflow/compat.lua
@@ -14,9 +14,10 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-local M = {}
-
 local buffers = require('mkdnflow.buffers')
+local init = require('mkdnflow.init')
+
+local M = {}
 
 M.goBack = function()
     buffers.goBack()
@@ -26,6 +27,28 @@ end
 M.goForward = function()
     buffers.goForward()
     print("⬇️ : WARNING - files.lua in mkdnflow is deprecated. Please replace any references to `files` in your config with references to `buffers` (e.g. change `require('mkdnflow').files.goForward()` to `require('mkdnflow').buffers.goForward()`).")
+end
+
+M.userConfigCheck = function(user_config)
+    if user_config.links_relative_to then
+        if type(user_config.links_relative_to) ~= 'table' then
+            print('⬇️ : WARNING - the links_relative_to key in the table passed to the mkdnflow setup function should now be associated with a table value. See :h mkdnflow-recent_changes')
+            if user_config.links_relative_to == 'current' then
+                local table = {
+                    target = 'current',
+                    fallback = 'first'
+                }
+                user_config.links_relative_to = table
+            elseif user_config.links_relative_to == 'first' then
+                local table = {
+                    target = 'first',
+                    fallback = 'current'
+                }
+                user_config.links_relative_to = table
+            end
+        end
+    end
+    return(user_config)
 end
 
 return M
