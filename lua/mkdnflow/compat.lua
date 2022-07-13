6d985afff4ddd9874a7a8333e64fc039e35fd8f5
jakewvincent <jakewvincent@gmail.com>
2022-07-12 19:03:09 -0700

    Remove old check for followPath(); spacing between conditional chunks for readability

--- lua/mkdnflow/compat.lua
+++ lua/mkdnflow/compat.lua
@@ -26,19 +26,12 @@ end
 local M = {}
 
 --[[
-followPath()
---]]
-M.followPath = function(path)
-    require('mkdnflow.links').followLink(path)
-    warn('⬇️  The use of followPath() will soon stop working. Please use followLink() instead. See :h mkdnflow-changes, commit c1cf25...')
-end
-
---[[
 userConfigCheck() will check a user config for particular settings that have
 since been migrated to another setting or another format. It returns an equiva-
 lent user config that is upgraded to the new format.
 --]]
 M.userConfigCheck = function(user_config)
+
     -- Check if to-do symbols are being customized but no values were provided
     -- for not_started, in_progress, and complete
     if user_config.to_do then
@@ -69,6 +62,7 @@ M.userConfigCheck = function(user_config)
             end
         end
     end
+
     -- Look for default bib path
     if user_config.default_bib_path then
         if user_config.default_bib_path == '' then
@@ -78,16 +72,19 @@ M.userConfigCheck = function(user_config)
         end
         warn('⬇️  The default_bib_path key has now been migrated into the bib key under the default_path option. Please update your config. See :h mkdnflow-changes, commit e9f7815...')
     end
+
     -- Look for link style
     if user_config.link_style then
         user_config.links.style = user_config.link_style
     end
+
     -- Look for implicit extension and remove periods
     if user_config.links then
         if user_config.links.implicit_extension then
             user_config.links.implicit_extension = string.gsub(user_config.links.implicit_extension, '%.', '')
         end
     end
+
     -- Look for oldest prefix settings
     if user_config.evaluate_prefix or user_config.new_file_prefix then
         user_config.prefix = {
@@ -95,6 +92,7 @@ M.userConfigCheck = function(user_config)
             string = user_config.new_file_prefix
         }
     end
+
     -- Look for old prefix settings
     if user_config.prefix then
         if user_config.prefix.evaluate == nil then
@@ -105,16 +103,19 @@ M.userConfigCheck = function(user_config)
         end
         warn('⬇️  The prefix key has now been migrated into the links key under the transform_explicit option. Please update your config. See :h mkdnflow-changes, commit f054440...')
     end
+
     -- Look for links_relative_to
     if user_config.links_relative_to then
         user_config.perspective = user_config.links_relative_to
         warn('⬇️  The links_relative_to key is now called "perspective". Please update your config. See :h mkdnflow-changes, commit e42290...')
     end
+
     -- Look for wrap preferences
     if user_config.wrap_to_beginning or user_config.wrap_to_end then
         user_config.wrap = true
         warn('⬇️  The wrap_to_beginning/end keys have been merged into a single "wrap" key. Please update your config. See :h mkdnflow-changes, commit 9068e1...')
     end
+
     -- Inspect perspective setting, if specified
     if user_config.perspective then
         if type(user_config.perspective) ~= 'table' then
@@ -137,6 +138,7 @@ M.userConfigCheck = function(user_config)
             user_config.perspective['nvim_wd_heel'] = user_config.perspective.vimwd_heel
         end
     end
+
     -- Inspect mappings
     if user_config.mappings then
         local string = false
