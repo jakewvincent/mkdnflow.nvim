7d3b062508cd78e84a5b2a14ba10010cf2078643
jakewvincent <jakewvincent@gmail.com>
2022-08-27 17:38:20 -0700

    Don't add space where there is a linebreak in links

--- lua/mkdnflow/links.lua
+++ lua/mkdnflow/links.lua
@@ -47,12 +47,12 @@ M.getLinkUnderCursor = function(col)
         local line_count, col_diff = vim.api.nvim_buf_line_count(0), 0
         for i = 1, config.links.context, 1 do
             local preceding_line = vim.api.nvim_buf_get_lines(0, row - 1 - i, row - 1, false)[1]
-            line = (preceding_line and preceding_line..' '..line) or line
+            line = (preceding_line and preceding_line..line) or line
             col_diff = preceding_line and (col_diff + #preceding_line)
             local following_line = vim.api.nvim_buf_get_lines(0, row, row + i, false)[1]
-            line = (following_line and line..' '..following_line) or line
+            line = (following_line and line..following_line) or line
         end
-        col = (col_diff and col + col_diff + 1) or col
+        col = (col_diff and col + col_diff) or col
     end
     -- Iterate through the patterns to see if there's a matching link under the cursor
     for link_type, pattern in pairs(patterns) do
@@ -615,7 +615,6 @@ M.followLink = function(path, anchor)
     if path or anchor then
         path, anchor = path, anchor
     else
-        vim.pretty_print(M.getLinkUnderCursor())
         path, anchor, link_type = M.getLinkPart(M.getLinkUnderCursor(), 'source')
     end
     if path then
