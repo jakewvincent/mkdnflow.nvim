7cfc0b36fc625c9d20a9278ab66924ac8e4a2979
jakewvincent <jakewvincent@gmail.com>
2023-07-04 17:18:31 -0700

    Comments

--- lua/mkdnflow/links.lua
+++ lua/mkdnflow/links.lua
@@ -481,6 +481,7 @@ M.formatLink = function(text, source, part)
         path_text = '#'..utf8.lower(path_text)
     elseif not source then
         path_text = M.transformPath(text)
+        -- If no path_text, end here
         if not path_text then return end
         if not links.implicit_extension then
             path_text = path_text..'.md'
@@ -545,6 +546,7 @@ M.createLink = function(from_clipboard)
             else
                 replacement = M.formatLink(cursor_word)
             end
+            -- If there's no replacement, stop here
             if not replacement then return end
             -- Find the (first) position of the matched word in the line
             local left, right = utf8.find(line, cursor_word, nil, true)
@@ -601,6 +603,7 @@ M.createLink = function(from_clipboard)
         else
             replacement = M.formatLink(text)
         end
+        -- If no replacement, end here
         if not replacement then return end
         -- Replace the visual selection w/ the formatted link replacement
         vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, replacement)
