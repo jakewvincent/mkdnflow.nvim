c6c59d44d02e4853da5b55c1f063fb68cae6206a
jakewvincent <jakewvincent@gmail.com>
2024-06-19 14:51:33 -0700

    build: Add script to convert xcf (GIMP) files to png for GitHub README

---
+++ scripts/xcf_to_png.sh
@@ -0,0 +1,31 @@
+# xcf_to_png.sh
+#!/bin/bash
+
+# Find XCF files that have been modified more recently than their corresponding PNG files
+xcf_files_to_convert=$(find . -name '*.xcf' | while read xcf_file; do
+    png_file="${xcf_file%.xcf}.png"
+    if [ ! -f "$png_file" ] || [ "$xcf_file" -nt "$png_file" ]; then
+        echo "$xcf_file"
+    fi
+done)
+
+# Convert the XCF files to PNG
+for file in $xcf_files_to_convert; do
+    if [ -f "$file" ]; then
+        dir_name=$(dirname "$file")
+        base_name=$(basename "$file" .xcf)
+        output_file="$dir_name/$base_name.png"
+        gimp -i -b "
+            (let* (
+                    (image (car (gimp-file-load RUN-NONINTERACTIVE \"$file\" \"$file\")))
+                    (drawable (car (gimp-image-get-active-layer image)))
+                )
+                (file-png-save RUN-NONINTERACTIVE image drawable \"$output_file\" \"$output_file\" 0 9 0 0 0 0 0)
+                (gimp-image-delete image)
+            )
+            (gimp-quit 0)
+        "
+        echo "  Converted $file to $base_name.png"
+    fi
+done
+
